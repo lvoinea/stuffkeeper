184fcb8910897af2fe4829eceb6d18c15784f790
Lucian Voinea <lucian.voinea@gmail.com>
2023-08-30 10:11:45 +0200

    Complete implementation for multiedit location.

--- frontend/src/views/Items.js
+++ frontend/src/views/Items.js
@@ -4,6 +4,7 @@ import { useNavigate } from "react-router-dom";
 
 import Autocomplete from '@mui/material/Autocomplete';
 import Backdrop from '@mui/material/Backdrop';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Checkbox from '@mui/material/Checkbox';
 import Chip from '@mui/material/Chip';
@@ -33,19 +34,20 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
 import UnarchiveIcon from '@mui/icons-material/Unarchive';
 
-import {getItems, addItem, saveItem, checkTag } from '../services/backend';
-import {setSelectedItem, setYItems, setVisibleStats, setIsMultiEdit} from '../services/store';
+import {getItems, addItem, saveItem, checkTag, checkLocation } from '../services/backend';
+import {setSelectedItem, setYItems, setVisibleStats, setIsMultiEdit,
+ setTags as setGlobalTags, setLocations as setGlobalLocations} from '../services/store';
 
 
 export default function Items() {
 
   const [items, setItems] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [isBusy, setIsBusy] = useState(false);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [editSelection, setEditSelection] = useState({});
   const [isMoveOpen, setIsMoveOpen] = useState(false);
   const [isTagOpen, setIsTagOpen] = useState(false);
-  const [isSaving, setIsSaving] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
   const [tags, setTags] = useState([]);
   const [locations, setLocations] = useState([]);
   const [inputTag, setInputTag] = useState('');
@@ -71,27 +73,6 @@ export default function Items() {
         return 'no-image-icon.gif';
     }
   }
-
-  const onAddItem = useCallback(async () => {
-    const newItem = {
-      "name": "",
-      "description": "",
-      "quantity": 1,
-      "cost": 0,
-      "photos": {
-        "sources": []
-      }
-    }
-    const addedItem = await addItem({token, item: newItem});
-    dispatch(setSelectedItem({name: addedItem.name, id: addedItem.id}));
-    dispatch(setYItems(window.pageYOffset));
-    navigate(`/items/${addedItem.id}/edit`);
-  }, [dispatch, navigate, token]);
-
-  const onMultiEdit = useCallback(async () => {
-    dispatch(setIsMultiEdit(true));
-    setIsMenuOpen(false);
-  }, [dispatch]);
 
   const isVisible = useCallback((item) => {
     let visible = (itemCategory === 'active' && item.is_active) ||
@@ -127,15 +108,11 @@ export default function Items() {
     return visible
   }, [itemCategory, searchFilter]);
 
-  const onMenuOpen = (event) => {
-    if (event.type === 'click'){
-        setIsMenuOpen(true);
-    }
-  };
-
-  const onMenuClose = () => {
-    setIsMenuOpen(false);
-  };
+  //--- Selecting
+  
+  const isItemsSelected = useCallback(() => {
+    return Object.values(editSelection).some(item => item);
+  }, [editSelection]);
 
   const onSelectionChange = (itemId) => () => {
     setEditSelection({
@@ -158,7 +135,7 @@ export default function Items() {
     setEditSelection(l_editSelection);
   },[editSelection]);
 
-  const handleSelectItem = (item) => () => {
+  const onSelectItem = (item) => () => {
     if (isMultiEdit) {
         onSelectionChange(item.id)();
     }
@@ -168,6 +145,26 @@ export default function Items() {
         navigate(`/items/${item.id}`);
     };
   };
+  
+  //--- Creating
+  
+  const onAddItem = useCallback(async () => {
+    const newItem = {
+      "name": "",
+      "description": "",
+      "quantity": 1,
+      "cost": 0,
+      "photos": {
+        "sources": []
+      }
+    }
+    const addedItem = await addItem({token, item: newItem});
+    dispatch(setSelectedItem({name: addedItem.name, id: addedItem.id}));
+    dispatch(setYItems(window.pageYOffset));
+    navigate(`/items/${addedItem.id}/edit`);
+  }, [dispatch, navigate, token]);
+  
+  //--- Tagging
 
   const getCommonTags = useCallback(() => {
 
@@ -219,7 +216,8 @@ export default function Items() {
 
     let needsSaving = (tagsAdded.length > 0) || (tagsDeleted.length > 0) ;
     if (needsSaving){
-        setIsSaving(true);
+        setIsDisabled(true);
+        setIsBusy(true);
         items.forEach(async (item) => {
             if (editSelection[item.id]) {
                 let tagsExisting = item.tags.map(tag => tag.name);
@@ -231,41 +229,113 @@ export default function Items() {
                     tags: tagsItem.map(tag => {
                         return {name: tag}
                     })
-                }
+                };
 
                 // Save the item itself
                 await saveItem({token, item: updatedItem, id: item.id});
                 // Update cached tags if new ones are added
-                const newGlobalTags = updatedItem.tags?.filter(tag => !checkTag(tag.name, tags));
+                const newGlobalTags = updatedItem.tags?.filter(tag => !checkTag(tag.name, globalTags));
                 if (newGlobalTags) {
-                    dispatch(setTags(tags.concat(newGlobalTags)));
+                    dispatch(setGlobalTags(globalTags.concat(newGlobalTags)));
                 }
             }
         });
-        setIsSaving(false);
+        setIsBusy(false);
+        setIsDisabled(false);
     };
     setTags([]);
     setIsTagOpen(false);
-  }, [token, items, tags, getCommonTags, editSelection, dispatch]);
+  }, [token, items, tags, getCommonTags, globalTags, editSelection, dispatch]);
 
-  const onMoveOpen = () => {
+  //--- Moving
+
+  const getCommonLocations = useCallback(() => {
+
+    // Get locations from the selected items
+    let selectedCount = 0;
+    let l_locations = {}
+    items.forEach(item => {
+        if (editSelection[item.id]) {
+            selectedCount += 1;
+            for(let i=0; i<item.locations.length; i++){
+                let locationName = item.locations[i].name
+                l_locations[locationName] = l_locations[locationName] || 0;
+                l_locations[locationName] += 1;
+            }
+        }
+    });
+
+    // Extract locations that occur in all selected items
+    let commonLocations=[];
+    Object.entries(l_locations).forEach(location => {
+        if (location[1] === selectedCount) {
+            commonLocations.push({
+                name: location[0]
+            });
+        }
+    });
+
+    return commonLocations;
+  },[items, editSelection]);
+
+  const onMoveOpen = useCallback(() => {
      setIsMenuOpen(false);
+     let commonLocations = getCommonLocations();
+     setLocations(commonLocations);
      setIsMoveOpen(true);
-  };
+  }, [getCommonLocations]);
 
   const onMoveClose = () => {
     setIsMoveOpen(false);
   };
 
-  const onMoveExecute = async (event) => {
+  const onMoveExecute = useCallback(async (event) => {
     event.preventDefault();
-    let needsSaving = false;
+
+    let locationsBefore = getCommonLocations().map(location=> location.name);
+    let locationsAfter = locations.map(location => location.name);
+    let locationsDeleted = locationsBefore.filter(location => !locationsAfter.includes(location));
+    let locationsAdded = locationsAfter.filter(location => !locationsBefore.includes(location));
+
+    let needsSaving = (locationsAdded.length > 0) || (locationsDeleted.length > 0) ;
     if (needsSaving){
-        setIsSaving(true);
-        setIsSaving(false);
+        setIsDisabled(true);
+        setIsBusy(true);
+        items.forEach(async (item) => {
+            if (editSelection[item.id]) {
+                let locationsExisting = item.locations.map(location => location.name);
+                let locationsItem = locationsExisting.filter( location => !locationsDeleted.includes(location));
+                let locationsNew = locationsAdded.filter( location => !locationsExisting.includes(location));
+                locationsItem.push(...locationsNew);
+
+                let updatedItem = {
+                    locations: locationsItem.map(location => {
+                        return {name: location}
+                    })
+                };
+
+                // Save the item itself
+                await saveItem({token, item: updatedItem, id: item.id});
+                // Update cached locations if new ones are added
+                const newGlobalLocations = updatedItem.locations?.filter(location => !checkLocation(location.name, globalLocations));
+                if (newGlobalLocations) {
+                    dispatch(setGlobalLocations(globalLocations.concat(newGlobalLocations)));
+                }
+            }
+        });
+        setIsBusy(false);
+        setIsDisabled(false);
     };
+    setTags([]);
     setIsMoveOpen(false);
-  };
+  }, [token, items, locations, getCommonLocations, globalLocations,editSelection, dispatch]);
+
+  //--- Other
+
+  const onMultiEdit = useCallback(async () => {
+    dispatch(setIsMultiEdit(true));
+    setIsMenuOpen(false);
+  }, [dispatch]);
 
   const onRestoreSelection = () => {
      setIsMenuOpen(false);
@@ -275,9 +345,57 @@ export default function Items() {
      setIsMenuOpen(false);
   };
 
-  const isItemsSelected = useCallback(() => {
-    return Object.values(editSelection).some(item => item);
-  }, [editSelection]);
+  //--- Action Menu
+
+  const onActionMenuOpen = (event) => {
+    if (event.type === 'click'){
+        setIsMenuOpen(true);
+    }
+  };
+
+  const onActionMenuClose = () => {
+    setIsMenuOpen(false);
+  };
+
+  const getActions = () => {
+
+        // Set-up scenario dependent speed dial actions
+        let actions = [];
+        if (!isMultiEdit) {
+            actions.push({ icon: <AddIcon />, name: 'Add new', action: onAddItem, static: true });
+            actions.push({ icon: <ModeEditOutlineOutlinedIcon />, name: 'Edit', action: onMultiEdit, static: true });
+        }
+
+        else {
+            if (itemCategory === 'active') {
+                actions.push({ icon: <InventoryIcon sx={{color: "#a10666"}}/>, name: 'Archive', action: onClearAll, static: false });
+            }
+            else {
+                actions.push({
+                    icon: <UnarchiveIcon sx={{color: "#1f750f"}}/>,
+                    name: 'Restore',
+                    action: onRestoreSelection,
+                    static: false
+                });
+                actions.push({
+                        icon: <DeleteIcon sx={{color: "#b52902"}}/>,
+                        name: 'Delete',
+                        action: onDeleteSelection,
+                        static: false
+                });
+            }
+
+            actions.push({ icon: <RemoveDoneIcon />, name: 'Clear all', action: onClearAll, static: true });
+            actions.push({ icon: <DoneAllIcon />, name: 'Select all', action: onSelectAll, static: true });
+            actions.push({ icon: <LaunchIcon />, name: 'Move', action: onMoveOpen, static: false });
+            actions.push({ icon: <LocalOfferIcon />, name: 'Tag', action: onTagOpen, static: false });
+
+        };
+
+        return actions;
+  };
+
+  //--- Set-up
 
   useEffect(() => {
     async function fetchData() {
@@ -328,48 +446,10 @@ export default function Items() {
     };
 
     // Load item data
-    setLoading(true);
+    setIsBusy(true);
     fetchData()
-    .finally(()=> {setLoading(false)});
+    .finally(()=> {setIsBusy(false)});
   }, [token, scrollPosition, isVisible, dispatch]);
-
-  const getActions = () => {
-
-        // Set-up scenario dependent speed dial actions
-        let actions = [];
-        if (!isMultiEdit) {
-            actions.push({ icon: <AddIcon />, name: 'Add new', action: onAddItem, static: true });
-            actions.push({ icon: <ModeEditOutlineOutlinedIcon />, name: 'Edit', action: onMultiEdit, static: true });
-        }
-
-        else {
-            if (itemCategory === 'active') {
-                actions.push({ icon: <InventoryIcon sx={{color: "#a10666"}}/>, name: 'Archive', action: onClearAll, static: false });
-            }
-            else {
-                actions.push({
-                    icon: <UnarchiveIcon sx={{color: "#1f750f"}}/>,
-                    name: 'Restore',
-                    action: onRestoreSelection,
-                    static: false
-                });
-                actions.push({
-                        icon: <DeleteIcon sx={{color: "#b52902"}}/>,
-                        name: 'Delete',
-                        action: onDeleteSelection,
-                        static: false
-                });
-            }
-
-            actions.push({ icon: <RemoveDoneIcon />, name: 'Clear all', action: onClearAll, static: true });
-            actions.push({ icon: <DoneAllIcon />, name: 'Select all', action: onSelectAll, static: true });
-            actions.push({ icon: <LaunchIcon />, name: 'Move', action: onMoveOpen, static: false });
-            actions.push({ icon: <LocalOfferIcon />, name: 'Tag', action: onTagOpen, static: false });
-
-        };
-
-        return actions;
-  };
 
   return(
     <React.Fragment>
@@ -377,7 +457,7 @@ export default function Items() {
      {/*------------------------------------------ Loading ------- */}
      <Backdrop
         sx={{ color: '#2c5585', backgroundColor: 'rgba(0, 0, 0, 0.1);', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={loading}
+        open={isBusy}
      >
         <CircularProgress color="inherit" thickness={8} />
      </Backdrop>
@@ -394,7 +474,7 @@ export default function Items() {
             sx={{ marginTop: 2}}/>
           }
           <ListItem disablePadding>
-            <ListItemButton role={undefined} onClick={handleSelectItem(item)}
+            <ListItemButton role={undefined} onClick={onSelectItem(item)}
                     alignItems="flex-start"
                     selected={item.id === currentItem.id}
                     dense>
@@ -436,11 +516,11 @@ export default function Items() {
                   }
                />
                {/*---------------- Locations --------------*/}
-               <Stack direction="row" spacing={1}>
+               <Box>
                     {item.locations.map(location =>
-                        <Chip key={item.id+location.name} label={location.name} size="small"/>
+                        <Chip key={item.id+location.name} label={location.name} size="small" sx={{marginTop: 0.5, marginRight: 1}}/>
                     )}
-               </Stack>
+               </Box>
             </Stack>
 
           </ListItemButton>
@@ -454,7 +534,7 @@ export default function Items() {
 
     {/*------------------------------------------- Actions ---------------*/}
     <SpeedDial
-            open={isMenuOpen} onOpen={onMenuOpen} onClose={onMenuClose}
+            open={isMenuOpen} onOpen={onActionMenuOpen} onClose={onActionMenuClose}
             ariaLabel="SpeedDial basic example"
             sx={{ position: 'fixed', bottom: 16, right: 16 }}
             icon={<MoreVertIcon />} >
@@ -473,14 +553,14 @@ export default function Items() {
     {/*------------------------------------------- Move ---------------*/}
     <Dialog open={isMoveOpen} onClose={onMoveClose}>
         <DialogTitle>Move selected items</DialogTitle>
-        <form onSubmit={onMoveExecute} disabled={isSaving}>
+        <form onSubmit={onMoveExecute} disabled={isDisabled}>
         </form>
     </Dialog>
 
     {/*------------------------------------------- Tag ---------------*/}
     <Dialog open={isTagOpen} onClose={onTagClose} >
         <DialogTitle>Tag selected items</DialogTitle>
-        <form onSubmit={onTagExecute} disabled={isSaving}>
+        <form onSubmit={onTagExecute} disabled={isDisabled}>
             <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{margin: 1.5}}>
             <Autocomplete name="tags"
                 multiple fullWidth freeSolo disableClearable
@@ -525,8 +605,57 @@ export default function Items() {
             <Button variant="outlined" type="submit">Tag</Button>
             </Stack>
         </form>
-     </Dialog>
+    </Dialog>
 
+    {/*------------------------------------------- Move ---------------*/}
+    <Dialog open={isMoveOpen} onClose={onMoveClose} >
+        <DialogTitle>Move selected items</DialogTitle>
+        <form onSubmit={onMoveExecute} disabled={isDisabled}>
+            <Stack direction="column" justifyContent="center" alignItems="center" spacing={2} sx={{margin: 1.5}}>
+            <Autocomplete name="locations"
+                multiple fullWidth freeSolo disableClearable
+                options={globalLocations}
+                getOptionLabel={(option) => option.name}
+                value={locations || []}
+                onChange={(event, newValue) => {
+                    setLocations(newValue);
+                }}
+                inputValue = {inputLocation}
+                onInputChange={(event, newValue) => {
+                    let candidateValue = newValue.trim();
+                    if (newValue !== candidateValue) {
+                        candidateValue = candidateValue.toLowerCase();
+                        if ((candidateValue !== '') && (!checkLocation(candidateValue, locations))) {
+                            const selectedLocations = locations.concat({name: candidateValue});
+                            setLocations(selectedLocations);
+                        }
+                        setInputLocation('');
+                    } else {
+                        setInputLocation(newValue);
+                    }
+                }}
+                isOptionEqualToValue={(option,value) => {
+                    return option.name === value.name;
+                }}
+                renderInput={(params) => (
+                  <TextField {...params} variant="filled" label="Locations"/>
+                )}
+                renderTags={(value, getLocationProps) =>
+                    value.map((option, index) => (
+                        <Chip
+                            color={checkLocation(option.name, globalLocations) ? 'info' : 'warning'}
+                            variant="filled"
+                            label={option.name}
+                            {...getLocationProps({ index })}
+                        />
+                    ))
+                }
+
+            />
+            <Button variant="outlined" type="submit">Move</Button>
+            </Stack>
+        </form>
+      </Dialog>
 
     </React.Fragment>
   );
